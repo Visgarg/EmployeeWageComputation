93ac0694d8791ada2b9fcf50ecb2abc272e99848
Vishal Garg <vishugarg10004@gmail.com>
2020-10-03 15:49:42 +0530

    [Vishal] refactor code to calculate monthly wage

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace EmployeeWageComputation
     {
         const int IS_PRESENT = 1;
         const int IS_PART_TIME = 2;
+        
         //const int IS_ABSENT = 0;
         NLog nLog = new NLog();
         /// <summary>
@@ -16,45 +17,67 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             NLog nLog = new NLog();
-
-            int wagePerHour = 20;
-            int fullDayHour;
-
             Console.WriteLine("Welcome to Employee Wage computation Program");
             nLog.LogInfo("Welcome message displayed : Main()");
-            //using Random method to generate random no and compare with constants to check presence.
-            Random random = new Random();
-            int checkIfPresent = random.Next(0, 3);
-            // instatiating program class and calling WorkHours method which returns no of hours according to employee's presence.
+            //instatiating program class for calling calculatemonthlywage method to calculate monthlywage.
             Program program = new Program();
-            fullDayHour = program.WorkHours(checkIfPresent);
-            int totalDailyWage = fullDayHour * wagePerHour;
-            Console.WriteLine("Total daily wage is " + totalDailyWage);
-            nLog.LogDebug("Successfully Calculated daily employee wage : Main()");
-
-
-
+            program.CalculateMonthlyWage();
 
         }
+        /// <summary>
+        /// calculating monthly wages of employee
+        /// </summary>
+        public void CalculateMonthlyWage()
+        {
+            int wagePerHour = 20;
+            int fullDayHour;
+            int workDayCount = 0;
+            int noOfWorkingDays = 20;
+            int totalWorkingHours = 0;
+            int totalDailyWage;
+            int monthlyWage;
+            //calculating total monthly wage by applying while loop on working days
+            while(workDayCount<noOfWorkingDays)
+            {
+                //using Random method to generate random no and compare with constants to check presence.
+                Random random = new Random();
+                int checkIfPresent = random.Next(0, 3);
+                // instatiating program class and calling WorkHours method which returns no of hours according to employee's presence.
+                Program program = new Program();
+                fullDayHour = program.WorkHours(checkIfPresent,workDayCount);
+                totalDailyWage = fullDayHour * wagePerHour;
+                Console.WriteLine($"Total daily wage is for {workDayCount+1} is {totalDailyWage}");
+                totalWorkingHours += fullDayHour;
+                workDayCount++;
+                //.LogDebug("Successfully Calculated daily employee wage : Main()");
+            }
+            monthlyWage = totalWorkingHours * wagePerHour;
+            nLog.LogDebug("Successfully calculated monthly employee wage: calculateMonthlyWage()");
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("The monthly wage of an employee is " + monthlyWage);
+        }
+
+
+
         /// <summary>
         /// Calculating Work hours after checking presence of employee.
         /// </summary>
         /// <param name="checkIfPresent"></param>
-        /// <returns></returns>
-        public int WorkHours(int checkIfPresent)
+        /// <returns>int emphours</returns>
+        public int WorkHours(int checkIfPresent,int workDayCount)
         {
             switch (checkIfPresent)
             {
                 case IS_PRESENT:
-                    Console.WriteLine("Employee is present");
+                    Console.WriteLine($"Employee is present on {workDayCount+1} day.");
                     nLog.LogDebug("Debug successful, for employee presence: WorkHours()");
                     return 8;
                 case IS_PART_TIME:
-                    Console.WriteLine("Employee is present for part time");
+                    Console.WriteLine($"Employee is present for part time on {workDayCount+1} day.");
                     nLog.LogDebug("Debug successful, for employee presence: WorkHours()");
                     return 4;
                 default:
-                    Console.WriteLine("Employee is Absent");
+                    Console.WriteLine($"Employee is Absent on {workDayCount+1}");
                     nLog.LogDebug("Debug successful, for employee presence: WorkHours()");
                     return 0;
             }
